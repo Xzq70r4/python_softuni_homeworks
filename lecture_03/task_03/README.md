568a7cbe2cbbbc6bb4415b83f88b41f9bf7b64d8
xzq70r4 <xzq70r4@gmail.com>
2017-01-17 00:31:47 +0200

    Write task_03 from lecture_03

---
+++ lecture_03/task_03/README.md
@@ -0,0 +1,11 @@
+### Task
+Up price from files in assets directory with 75% and save in another file.
+
+###CVS structure:
+ 1. Catalog number
+ 2. Product name
+ 3. Product color/colors
+ 4. Kind of activity
+ 5. Group of product
+ 6. Gender and Age Group (Men, Women, Unisex, ...)
+ 7. Price
---
+++ lecture_03/task_03/task_03.py
@@ -0,0 +1,14 @@
+FILE_PATH = './../assets/catalog_sample.csv'  # or catalog_full.csv
+UPDATED_FILE_PATH = './../assets/updated_price_catalog_sample.csv'
+PERCENT = 0.75   #  75%
+
+with open(FILE_PATH, encoding='utf-8') as f:
+    with open(UPDATED_FILE_PATH, 'w', encoding='utf-8') as f_updated:
+        for line in f:
+            line = line.strip()
+            current_line_list = line.split(',')
+            updated_price = float(current_line_list[-1]) +\
+                            float(current_line_list[-1]) * PERCENT
+            current_line_list[-1] = updated_price
+            updated_line = ','.join(str(e) for e in current_line_list)
+            f_updated.write(updated_line + '\n')
